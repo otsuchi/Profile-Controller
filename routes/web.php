ffba43358cbf16d19756cfdbec286497fb19361a
Tsuchiya Yuki <ottuchiyan7128@gmail.com>
2022-03-16 10:48:22 +0000

    second laravel9

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create',
-'Admin\NewsController@add');
+    Route::get('news/create','Admin\NewsController@add');
+Route::get('profile/create','Admin\ProfileController@add');
+});
+Route::get('profile/edit','Admin\ProfileController@edit');
 });
 Route::get('XXX','AAAController@bbb');
-Route::get('admin/profile/create','ProfileController@edit');
