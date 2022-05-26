c290e6e642bb8cd65fd3c6f28bafc196260a579a
Tsuchiya Yuki <ottuchiyan7128@gmail.com>
2022-05-26 21:10:37 +0000

    laravel19

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::group(['prefix' => 'admin','middleware' => 'auth'], function(){
     Route::get('profile/edit','Admin\ProfileController@edit');
     Route::post('profile/edit', 'Admin\ProfileController@update');
     Route::get('profiles/delete', 'Admin\ProfileController@delete');
+
+    
 });
 
 
@@ -39,4 +41,7 @@ Route::group(['prefix' => 'admin'], function() {
      Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
      Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+     
 });
+    Route::get('/', 'NewsController@index');
+    Route::get('/profile', 'ProfileController@index');
